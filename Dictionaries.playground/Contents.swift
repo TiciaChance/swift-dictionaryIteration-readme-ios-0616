c9061b8713ee140b314ea997cf1966c126b26aa5
TiciaChance <laticia.chance@flatironschool.com>
2016-07-10 20:40:07 -0400

    done

--- Dictionaries.playground/Contents.swift
+++ Dictionaries.playground/Contents.swift
@@ -1,72 +1,87 @@
-let airports = [
-    "YYZ": "Toronto Pearson",
-    "DUB": "Dublin",
-    "JFK": "John F. Kennedy International Airport",
-]
+let spikeLee = [ "Do the right thing", "crooklyn", "she's gotta have it", "jungle fever", "25th Hour", "school daze", "Malcolm X"]
+let martinScorsese = ["Goodfella", "The Wolf of Wall Street", "the departed", "taxi driver", "raging bull"]
+let quentinTarantino = ["django", "resevoir dogs", "kill bill", "inglorious bastards", "pulp fiction", "hateful eight", "jackie brown"]
 
-if let airport = airports["YYZ"] {
-    print("The abbreviation for \(airport) is YYZ")
-}
+var directorsAndMovies = ["Spike Lee": spikeLee, "Martin Scorsese": martinScorsese, "Quentin Tarantino":quentinTarantino]
 
-if let airport = airports["DUB"] {
-    print("The abbreviation for \(airport) is DUB")
-}
-
-if let airport = airports["JFK"] {
-    print("The abbreviation for \(airport) is JFK")
-}
-
-for (key, value) in airports {
-    print("The abbreviation for \(value) is \(key)")
-}
-
-for (airportCode, airportName) in airports {
-    print("The abbreviation for \(airportName) is \(airportCode)")
+for (director, movies) in directorsAndMovies {
+    print("\(director) directed all of these movies: \(movies)!")
+    print("\(director) made \(movies.count) movies and more!")
 }
 
 
-
-
-let toyStoryFilms = [
-    "Toy Story",
-    "Toy Story 2",
-    "Buzz Lightyear of Star Command: The Adventure Begins",
-    "Toy Story 3",
-    "Toy Story 4"
-]
-
-let starWarsFilms = [
-    "Star Wars",
-    "The Empire Strikes Back",
-    "Star Wars: Episode VI",
-    "Star Wars: Episode I",
-    "Star Wars: Episode II",
-    "Star Wars: Episode III",
-    "Star Wars: The Clone Wars",
-    "Star Wars: The Force Awakens",
-    "Star Wars: Episode VIII",
-    "Star Wars: Episode IX"
-]
-
-let fastAndFuriousFilms = [
-    "The Fast and the Furious",
-    "2 Fast 2 Furious",
-    "Turbo-Charged Prelud",
-    "Tokyo Drift",
-    "Fast & Furious",
-    "Los Bandoleros",
-    "Fast Five",
-    "Fast & Furious 6",
-    "Furious 7",
-    "Fast 8"
-]
-
-let movies = [
-    "Star Wars": starWarsFilms,
-    "Fast & Furious": fastAndFuriousFilms,
-    "Toy Story": toyStoryFilms,
-]
-
-for (franchise, titles) in movies {
-    print("There are \(titles.count) movies in the \(franchise) series")
-}
+//let airports = [
+//    "YYZ": "Toronto Pearson",
+//    "DUB": "Dublin",
+//    "JFK": "John F. Kennedy International Airport",
+//]
+//
+//if let airport = airports["YYZ"] {
+//    print("The abbreviation for \(airport) is YYZ")
+//}
+//
+//if let airport = airports["DUB"] {
+//    print("The abbreviation for \(airport) is DUB")
+//}
+//
+//if let airport = airports["JFK"] {
+//    print("The abbreviation for \(airport) is JFK")
+//}
+//
+//for (key, value) in airports {
+//    print("The abbreviation for \(value) is \(key)")
+//}
+//
+//for (airportCode, airportName) in airports {
+//    print("The abbreviation for \(airportName) is \(airportCode)")
+//}
+//
+//
+//
+//
+//let toyStoryFilms = [
+//    "Toy Story",
+//    "Toy Story 2",
+//    "Buzz Lightyear of Star Command: The Adventure Begins",
+//    "Toy Story 3",
+//    "Toy Story 4"
+//]
+//
+//let starWarsFilms = [
+//    "Star Wars",
+//    "The Empire Strikes Back",
+//    "Star Wars: Episode VI",
+//    "Star Wars: Episode I",
+//    "Star Wars: Episode II",
+//    "Star Wars: Episode III",
+//    "Star Wars: The Clone Wars",
+//    "Star Wars: The Force Awakens",
+//    "Star Wars: Episode VIII",
+//    "Star Wars: Episode IX"
+//]
+//
+//let fastAndFuriousFilms = [
+//    "The Fast and the Furious",
+//    "2 Fast 2 Furious",
+//    "Turbo-Charged Prelud",
+//    "Tokyo Drift",
+//    "Fast & Furious",
+//    "Los Bandoleros",
+//    "Fast Five",
+//    "Fast & Furious 6",
+//    "Furious 7",
+//    "Fast 8"
+//]
+//
+//let movies = [
+//    "Star Wars": starWarsFilms,
+//    "Fast & Furious": fastAndFuriousFilms,
+//    "Toy Story": toyStoryFilms,
+//]
+//
+//for (franchise, titles) in movies {
+//    print("There are \(titles.count) movies in the \(franchise) series")
+//}
+//
+//
+//
